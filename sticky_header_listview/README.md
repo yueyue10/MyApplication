0e99bb3edfa8a357737a91ba20722298e493a8b1
zhaoyj <1650432983@qq.com>
2018-11-22 20:46:50 +0800

    去掉多余的代码

--- sticky_header_listview/README.md
+++ sticky_header_listview/README.md
@@ -1,6 +1,4 @@
 # sticky_header_listview介绍
 
-<div>
 <h2>头部悬浮效果图------------------------------------</h2>
-<img src="https://raw.github.com/yueyue10/MyApplication/master/doc/stickyheaderlistview/效果图.gif" width="80%"  />
-</div>
+![头部悬浮效果图](../doc/stickyheaderlistview/效果图.gif)
--- sticky_header_listview/src/main/java/com/encdata/sticky_header_listview/MyAdapter.java
+++ sticky_header_listview/src/main/java/com/encdata/sticky_header_listview/MyAdapter.java
@@ -1,8 +1,5 @@
 package com.encdata.sticky_header_listview;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,21 +8,20 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SectionIndexer;
 import android.widget.TextView;
 
-import com.encdata.sticky_header_listview.view.StickyListHeadersAdapter;
 import com.encdata.sticky_header_listview.bean.DaHuiDate;
 import com.encdata.sticky_header_listview.bean.DaHuiInfo;
+import com.encdata.sticky_header_listview.view.StickyListHeadersAdapter;
+
+import java.util.List;
 
 
 public class MyAdapter extends BaseAdapter implements
-        StickyListHeadersAdapter, SectionIndexer {
+        StickyListHeadersAdapter {
 
     private String[] mCountries = {"aBC", "bar"};
     private List<DaHuiDate> datas; // 原始数据源
-    private int[] mSectionIndices;// 用来存放每一轮分组的第一个item的位置。
-    private String[] mSectionLetters;// 用来存放每一个分组要展现的数据
     private final Context context;
     private LayoutInflater mInflater;
 
@@ -33,35 +29,6 @@ public class MyAdapter extends BaseAdapter implements
         this.context = context;
         this.datas = datas;
         mInflater = LayoutInflater.from(context);
-        mSectionIndices = getSectionIndices();
-        mSectionLetters = getSectionLetters();
-    }
-
-    private int[] getSectionIndices() {
-        ArrayList<Integer> sectionIndices = new ArrayList<Integer>();
-        char lastFirstChar = mCountries[0].charAt(0);
-        sectionIndices.add(0);
-        for (int i = 1; i < mCountries.length; i++) {
-            if (mCountries[i].charAt(0) != lastFirstChar) {
-                lastFirstChar = mCountries[i].charAt(0);
-                sectionIndices.add(i);
-            }
-        }
-        int[] sections = new int[sectionIndices.size()];
-        for (int i = 0; i < sectionIndices.size(); i++) {
-            sections[i] = sectionIndices.get(i);
-        }
-        return sections;
-    }
-
-    private String[] getSectionLetters() {
-        String[] letters = new String[datas.size()];
-        if (datas != null) {
-            for (int i = 0; i < datas.size(); i++) {
-                letters[i] = datas.get(i).date + "  " + datas.get(i).week;
-            }
-        }
-        return letters;
     }
 
     @Override
@@ -160,31 +127,6 @@ public class MyAdapter extends BaseAdapter implements
     @Override
     public long getHeaderId(int position) {
         return position;
-    }
-
-    @Override
-    public int getPositionForSection(int section) {
-        if (section >= mSectionIndices.length) {
-            section = mSectionIndices.length - 1;
-        } else if (section < 0) {
-            section = 0;
-        }
-        return mSectionIndices[section];
-    }
-
-    @Override
-    public int getSectionForPosition(int position) {
-        for (int i = 0; i < mSectionIndices.length; i++) {
-            if (position < mSectionIndices[i]) {
-                return i - 1;
-            }
-        }
-        return mSectionIndices.length - 1;
-    }
-
-    @Override
-    public Object[] getSections() {
-        return mSectionLetters;
     }
 
     class HeaderViewHolder {
